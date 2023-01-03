9d25f7b75429750df60d46ee5ef0f2f9ce28585d
coutinhopmw <cassiocoutinho258@gmail.com>
2023-01-03 08:37:56 -0300

    Versão 01 do site

---
+++ site-git/scripts/script.js
@@ -0,0 +1,31 @@
+let count = 1;
+document.getElementById("slide01").checked = true;
+
+setInterval ( function() {
+    nextImage();
+}, 5000)
+
+function nextImage() {
+    count++;
+    if(count > 2){
+        count = 1;
+    }
+
+    document.getElementById("mobile-slide0"+count).checked = true;
+}
+
+let integer = 1;
+document.getElementById("mobile-slide01").checked = true;
+
+setInterval ( function() {
+    nextImage();
+}, 1000)
+
+function nextImage() {
+    integer++;
+    if(integer > 2){
+        integer = 1;
+    }
+
+    document.getElementById("mobile-slide0"+integer).checked = true;
+}
